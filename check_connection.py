0516f0bc74bf3c1234c6487e8c77b879c05e0067
El676 <153519768+El676@users.noreply.github.com>
2024-07-06 15:20:39 +0300

    Delete check_connection.py

--- check_connection.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-try:
-    # Подключение к базе данных
-    conn = psycopg2.connect(host='localhost', dbname='postgres', user='postgres', password='PASS')
-    print("Подключение к базе данных успешно")
-    conn.close()
-except Exception as e:
-    print(f"Ошибка подключения к базе данных: {e}")
